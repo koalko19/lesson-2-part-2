3eb692b6f41222b9a28022d58ca410d04b410577
koalko19 <rastrovskayaa@mail.ru>
2021-05-07 21:18:04 +0300

    commit

---
+++ src/L1p2t1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class L1p2t1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("Введите три значения" + "\nПолученное число:" + a + b + c);
+
+
+    }
+}
